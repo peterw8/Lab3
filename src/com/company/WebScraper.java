a25413006d06bfcf704983d77e8ee57f98f53d69
peterw8 <pbweiss2@illinois.edu>
2018-02-07 18:18:08 -0600

    finished wordcounter

--- src/com/company/WebScraper.java
+++ src/com/company/WebScraper.java
@@ -1,7 +1,34 @@
 package com.company;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
+
 public class WebScraper {
     public static void main(String[] unused) {
-        System.out.println("Run");
+        String webName = "http://erdani.com/tdpl/hamlet.txt";
+        String checker = urlToString(webName);
+        String[] total = new String [checker.length()];
+        total = checker.split("\\s+");
+
+        System.out.println(total.length);
+    }
+
+    /**
+     * Retrieve contents from a URL and return them as a string.
+     *
+     * @param url url to retrieve contents from
+     * @return the contents from the url as a string, or an empty string on error
+     */
+    public static String urlToString(final String url) {
+        Scanner urlScanner;
+        try {
+            urlScanner = new Scanner(new URL(url).openStream(), "UTF-8");
+        } catch (IOException e) {
+            return "";
+        }
+        String contents = urlScanner.useDelimiter("\\A").next();
+        urlScanner.close();
+        return contents;
     }
 }
